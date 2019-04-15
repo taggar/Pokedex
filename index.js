56643b2423d9e70b8776db7bf57e1e19b574395d
taggar <rvdeede@gmail.com>
2019-04-15 11:30:10 +0200

    Implemented with a promise for pokemon evolutions

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ function doSearch(e) {
     xhr.open("GET", target);
     xhr.responseType = "json";
     xhr.send();
-    xhr.onreadystatechange = function() {
     xhr.onreadystatechange = function () {
       if (xhr.readyState != 4) {
         console.log("xhr to " + target);
       }
       if (xhr.readyState == 4) {
-        // SetTimeout only to make the loading step visible in the exercise
         if (xhr.status !== 200) {
           rawData.innerText = xhr.status;
           return;
@@ -41,29 +39,86 @@ function doSearch(e) {
         /* Show moves
         ==============*/
         pokeMoves.innerText = JSON.stringify(xhr.response.moves, null, 5);
-        getEvolutions(xhr.response.species.url);
+
+        /* Show previous evolution(s)
+        =============================*/
+        //getEvolutions(xhr.response.species.url);
+
+
+        // console.log('now doXhrGetJson');
+        // //doXhrGetJson(xhr.response.species.url);
+        // let species = {};
+        // species = doXhrGetJson(xhr.response.species.url);
+        //   pokeEvo.innerText = species.evolves_from_species.name;
+
+
+        doXhrGetJson(xhr.response.species.url)
+          .then(function (result) {
+            pokeEvo.innerText = result.evolves_from_species.name;
+          })
+          .catch(function () {
+            pokeEvo.innerText = 'None found.'
+          });
       }
     };
   }
 }
 
-var getEvolutions = function getEvolutions(url) {
+function getEvolutions(url) {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", url);
   xhr.responseType = "json";
   xhr.send();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState != 4) {
       console.log("xhr to " + url);
     }
     if (xhr.readyState == 4) {
       try {
+        console.log(xhr.response);
         console.log(xhr.response.evolves_from_species.name);
         pokeEvo.innerText = xhr.response.evolves_from_species.name;
       } catch {
         pokeEvo.innerText = "";
       }
     }
-    // Xhr.response.species.evolves_from_species.name;
-  };
-};
+  }
+}
+
+/* Function to get an ajax json response
+=======================================*/
+// function doXhrGetJson(url) {
+//   let response;
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("GET", url);
+//   xhr.responseType = "json";
+//   xhr.send();
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState != 4) {
+//       console.log("xhr to " + url);
+//     }
+//     if (xhr.readyState == 4) {
+//       if (xhr.status !== 200) {
+//         console.log(xhr.status + ': ' + url);
+//         response = xhr.status;
+//       }
+//       console.log(xhr.response);
+//       response = xhr.response;
+//     }
+//     console.log('response from doXhrGetJson: ' + response)
+//     return response;
+//   };
+// }
+
+function doXhrGetJson(url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(this.response);
+    };
+    xhr.onerror = reject;
+    xhr.responseType = "json";
+    xhr.open('GET', url);
+    xhr.send();
+  });
+}
