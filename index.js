8ac9776fd9761c8667f33109a4ba98c707fc68df
taggar <rvdeede@gmail.com>
2019-04-15 12:37:22 +0200

    Some cleanup, styling, and some ES6 loop syntax

--- index.js
+++ index.js
@@ -11,48 +11,62 @@ searchBox.addEventListener('keyup', doSearch);
 
 function doSearch(e) {
   if (e.which === 13 && e.target.value.length > 0) {
-    let xhr = new XMLHttpRequest();
     let target = 'https://pokeapi.co/api/v2/pokemon/' + e.target.value;
-    xhr.open('GET', target);
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState != 4) {
-        console.log('xhr to ' + target);
-      }
-      if (xhr.readyState == 4) {
-        if (xhr.status !== 200) {
-          rawData.innerText = xhr.status;
-          return;
-        }
-        rawData.innerText = JSON.stringify(xhr.response, null, 5);
+    console.log('xhr to ' + target);
+    doXhrGetJson(target)
+      .then(function(result) {
+        console.log('Result: ' + result);
+        rawData.innerHTML = '<pre>' + JSON.stringify(result, null, 5) + '</pre>';
 
         /* Show ID and name
         ===================*/
-        pokeID.innerText = xhr.response.id + ', ' + xhr.response.name;
+        pokeID.innerText = result.id + ', ' + result.name;
 
         /* Show picture
         ===============*/
+        console.log('pic: ' + JSON.stringify(result.sprites.front_shiny, null, 5));
         pokePic.innerHTML =
-          '<img src=' + xhr.response.sprites.front_shiny + '>';
+          '<img src=' + result.sprites.front_shiny + '>';
 
         /* Show moves
         ==============*/
-        pokeMoves.innerText = JSON.stringify(xhr.response.moves, null, 5);
+        //Console.log('moves: ' + JSON.stringify(result.moves, null, 5));
+        let moves = [];
+        for (move of result.moves) {
+          moves.push(move.move.name);
+        }
+        // Console.log(move.move.name);
+        // pokeMoves.innerHTML += '<a href=\'' + move.move.url + '\'>' + move.move.name + '</a>';
+        // Moves.push(move.name);
+        // moves.join(move.name,', ');
+        // pokeMoves.innerHTML += '<pre>' + JSON.stringify(result.moves, null, 5) + '</pre>'
+        pokeMoves.innerHTML = moves.join(', ');
+        // PokeMoves.innerHTML = moves;
 
         /* Show previous evolution(s)
         =============================*/
         // Implemented with a Promise as we don't wait for the next ajax call to get back
-        doXhrGetJson(xhr.response.species.url)
-          .then(function(result) {
-            pokeEvo.innerText = result.evolves_from_species.name;
-          })
-          .catch(function() {
-            pokeEvo.innerText = 'None found.'
-          });
-      }
-    };
+        getEvolutions(result);
+
+      })
+      .catch(function() {
+        pokeID.innerText = 'Not found ...'
+        pokePic.innerHTML = '';
+        pokeMoves.innerText = '';
+        pokeEvo.innerText = '';
+
+      });
   }
+}
+
+function getEvolutions(result) {
+  doXhrGetJson(result.species.url)
+    .then(function(result) {
+      pokeEvo.innerText = result.evolves_from_species.name;
+    })
+    .catch(function() {
+      pokeEvo.innerText = 'None found.';
+    });
 }
 
 
